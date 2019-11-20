760705ffc4159341a44795c2147680aa87db887b
luisjulio1999 <luis_julio_1999@hotmail.com>
2019-11-20 17:02:47 -0400

    Inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,122 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+<title>{{ config('app.name', 'CTFDashboard') }} - Login</title>
+<meta charset="utf-8">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="Oculux Bootstrap 4x admin is super flexible, powerful, clean &amp; modern responsive admin dashboard with unlimited possibilities.">
+<meta name="author" content="GetBootstrap, design by: puffintheme.com">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+<!-- Styles -->
+<style>
+    body {
+        background-image: url("{{asset('images/worldback.jpg')}}");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    .full-height {
+        height: 100vh;
+    }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
 
-            .full-height {
-                height: 100vh;
-            }
+    .position-ref {
+        position: relative;
+    }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
 
-            .position-ref {
-                position: relative;
-            }
+    .content {
+        text-align: center;
+        width: 100%;
+    }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    .title {
+        font-size: 85px;
+    }
 
-            .content {
-                text-align: center;
-            }
+    .text {
+        color: #FEFEFE;
+        text-shadow: 1px 2px 0px rgba(0,134,213, 0.5);
+        font-family: 'Montserrat', sans-serif;
+    }
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
 
-            .title {
-                font-size: 84px;
-            }
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+</style>
+<!-- VENDOR CSS -->
+<link rel="stylesheet" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('vendor/font-awesome/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('vendor/animate-css/vivify.min.css')}}">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="{{asset('css/site.min.css')}}">
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+</head>
+<body class="theme-cyan font-montserrat">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+<!-- Page Loader -->
+<div class="page-loader-wrapper">
+    <div class="loader">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+        <div class="bar4"></div>
+        <div class="bar5"></div>
+    </div>
+</div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<div class="pattern">
+    <span class="red"></span>
+    <span class="indigo"></span>
+    <span class="blue"></span>
+    <span class="green"></span>
+    <span class="orange"></span>
+</div>
+    @if (Route::has('login'))
+        <div class="top-right links">
+                          
+            <a class="float-right text" href="{{ route('login') }}">Login</a>
+        
         </div>
-    </body>
+    @endif
+<div class="auth-main particles_js">            
+                    <div class="content">
+                        <h1 class="text m-b-md title">Plataforma CTF</h1>
+                    </div>
+    <div id="particles-js"></div>
+</div>
+<!-- END WRAPPER -->
+    
+<script src="{{asset('bundles/libscripts.bundle.js')}}"></script>    
+<script src="{{asset('bundles/vendorscripts.bundle.js')}}"></script>
+<script src="{{asset('bundles/mainscripts.bundle.js')}}"></script>
+<script src="{{asset('js/login.js')}}"></script>
+</body>
 </html>
