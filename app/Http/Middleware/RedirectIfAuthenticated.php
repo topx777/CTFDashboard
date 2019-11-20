7aa3ab5e13924df3066e16c77ac979b1abb63f37
topx777 <topx777@gmail.com>
2019-11-20 12:49:11 -0400

    Login de Usuario y Rutas de Auth

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            if (auth()->user()->admin == 1) {
+                return redirect('/admin/home');
+            } else {
+                return redirect('/team/dashboard');
+            }
         }
 
         return $next($request);
