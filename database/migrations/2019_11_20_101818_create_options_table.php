5b338d6e0d1f78cdde4fc5f2310307134662a609
MauricioGamarraVargas <46828503+MauricioGamarraVargas@users.noreply.github.com>
2019-11-20 11:08:59 -0400

    cambios mauricio migrations db

---
+++ database/migrations/2019_11_20_101818_create_options_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->boolean('state')->default(false);
+            $table->text('rules');
+            $table->dateTime('startTime');
+            $table->dateTime('endTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+}
