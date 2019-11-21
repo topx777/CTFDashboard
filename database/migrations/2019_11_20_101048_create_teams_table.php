0d7a7c029a269a8e857d6ea6cd89f45a181f5232
DkChriss <dk_matt_1_2@hotmail.com>
2019-11-21 11:26:21 -0400

    PasswordTeam

--- database/migrations/2019_11_20_101048_create_teams_table.php
+++ database/migrations/2019_11_20_101048_create_teams_table.php
@@ -17,10 +17,11 @@ class CreateTeamsTable extends Migration
             $table->bigIncrements('id');
             $table->biginteger('idUser')->unsigned();
             $table->string('name', 55);
-            $table->integer('score');
+            $table->integer('score')->default(0);
             $table->text('phrase')->nulleable();
             $table->string('avatar', 255)->nullable();
             $table->string('couch', 55)->nullable();
+            $table->string('teampassword')->nullable();
             $table->timestamps();
             $table->foreign('idUser')->references('id')->on('users');
         });
