007dde228140ac56704c42ea63e5a3f798df45d5
DkChriss <dk_matt_1_2@hotmail.com>
2019-11-25 10:05:55 -0400

    Tabla Files and migraciones

--- database/migrations/2019_11_25_092452_create_files_table.php
+++ database/migrations/2019_11_25_092452_create_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique()->nulleable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('size');
+            $table->string('ext');
+            $table->dateTime('upload_date');
+            $table->string('direction');
             $table->timestamps();
-            $table->boolean('admin')->default(false);
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('files');
     }
 }
