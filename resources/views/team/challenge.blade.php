4719e61a35aa8ba5d272de31cd9f4937f05abd98
MauricioGamarraVargas <46828503+MauricioGamarraVargas@users.noreply.github.com>
2019-11-26 10:12:28 -0400

    vista reto
    Co-authored-by: chrisshuayhua@hotmail.com <chrisshuayhua@hotmail.com>

--- resources/views/team/challenge.blade.php
+++ resources/views/team/challenge.blade.php
@@ -1,8 +1,34 @@
 @extends('teamLayout.master')
 @section('content')
-<div>
-    Esto va a servir para ver el reto 
+
+    <div class="col-lg-6">
+
+    
+<div class="card">
+    <div class="body">
+        <div id="slider2" class="carousel vert slide" data-ride="carousel" data-interval="1700">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="card-value float-right text-muted"><i class="wi wi-fog"></i></div>
+                    <h3 class="mb-1">12°C</h3>
+                    <div>London</div>
+                </div>
+                <div class="carousel-item">
+                    <div class="card-value float-right text-muted"><i class="wi wi-day-cloudy"></i></div>
+                    <h3 class="mb-1">18°C</h3>
+                    <div>New York</div>
+                </div>
+                <div class="carousel-item">
+                    <div class="card-value float-right text-muted"><i class="wi wi-sunrise"></i></div>
+                    <h3 class="mb-1">37°C</h3>
+                    <div>New Delhi</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+</div>
+
 @endsection 
 @section('script')
 @endsection
