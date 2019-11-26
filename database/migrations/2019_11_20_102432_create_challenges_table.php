674a8d63a4d81e33c36c9615c6279a0d4c1e33e1
MauricioGamarraVargas <46828503+MauricioGamarraVargas@users.noreply.github.com>
2019-11-26 11:47:21 -0400

    views retos

--- database/migrations/2019_11_20_102432_create_challenges_table.php
+++ database/migrations/2019_11_20_102432_create_challenges_table.php
@@ -21,6 +21,7 @@ class CreateChallengesTable extends Migration
             $table->text('description');
             $table->text('hint');
             $table->text('flag');
+            $table->integer('order');
             $table->timestamps();
             $table->foreign('idLevel')->references('id')->on('levels');
             $table->foreign('idCategory')->references('id')->on('categories');
--- resources/views/team/challenge.blade.php
+++ resources/views/team/challenge.blade.php
@@ -27,7 +27,7 @@
                 <div id="slider2" class="carousel vert slide" data-ride="carousel" data-interval="1700">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <div class="card-value float-right text-muted"><i class="icon-screen-desktop"></i></div>
+                            <div class="card-value float-right text-muted"><i class="fa fa-key"></i></div>
                             <h3 class="mb-1">CRIPTOGRAFIA</h3>
                         </div>
                     </div>
@@ -39,15 +39,90 @@
     <div class="body">
         <blockquote class="blockquote mb-0">
             <p>El reto consiste en saber cuanto se enbolsillo el evo con el juancito pinto gobierno para los ninos con el bomo jauncito pinto es para escuelas fiscales</p>
-            <footer class="blockquote-footer">
-         <label for="txt" class="mr-2">Ingrese la bandera
-        <i class="fa fa-flag"> :</i>     
-        </label> 
-         <input id="txt" type="text" class="text"/>
-                </footer>
         </blockquote>
     </div>
 </div>
+<div class="col-md-12">
+        <div class="card">
+            <div class="header">
+               
+                <ul class="header-dropdown dropdown">
+                    
+                    <li><a href="javascript:void(0);" class="full-screen"><i class="icon-frame"></i></a></li>
+                    <li class="dropdown">
+                        <ul class="dropdown-menu">
+                            <li><a href="javascript:void(0);">Action</a></li>
+                            <li><a href="javascript:void(0);">Another Action</a></li>
+                            <li><a href="javascript:void(0);">Something else</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="body">
+                <div class="table-responsive">
+                    <table class="table m-b-0">
+                        <thead class="thead-light">
+                            <tr>
+                            <th scope="col">#</th>
+                            <th></th>
+                            <th scope="col">Capturas</th>
+                            <th scope="col">puntos</th>
+                            <th scope="col">Ayuda</th>
+                            <th scope="col">Bandera</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Criptografia</td>
+                            <td>False</td>
+                            <td>100</td>
+                            <td>
+                                 <button class="btn btn-danger">
+                                    <i class="fa fa-ambulance fa-2x"></i>
+                                 </button>
+                            </td>
+                            <td><button class="btn btn-primary">
+                                <i class="fa fa-flag"></i>    
+                                -Enviar  
+                            </button></td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Reto 2</td>
+                            <td>False</td>
+                            <td>100</td>
+                            <td>
+                                    <button class="btn btn-danger">
+                                       <i class="fa fa-ambulance fa-2x"></i>
+                                    </button>
+                               </td>
+                               <td><button class="btn btn-primary">
+                                   <i class="fa fa-flag"></i>    
+                                   -Enviar  
+                               </button></td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td>Reto 3</td>
+                            <td>False</td>
+                            <td>100</td>
+                            <td>
+                                    <button class="btn btn-danger">
+                                       <i class="fa fa-ambulance fa-2x"></i>
+                                    </button>
+                               </td>
+                               <td><button class="btn btn-primary">
+                                   <i class="fa fa-flag"></i>    
+                                   -Enviar  
+                               </button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection 
 @section('script')
 @endsection
