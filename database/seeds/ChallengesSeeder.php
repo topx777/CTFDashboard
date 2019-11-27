75e56fd37eea47bf5ff66e47058b8038374c15b8
DkChriss <dk_matt_1_2@hotmail.com>
2019-11-27 11:10:48 -0400

    PUSHEO A LO LOCO

--- database/seeds/ChallengesSeeder.php
+++ database/seeds/ChallengesSeeder.php
@@ -16,46 +16,19 @@ class ChallengesSeeder extends Seeder
             'id' => 1,
             'idLevel' => 1,
             'idCategory' => 1,
-            'name' => 'Nombre del Reto',
-            'description' => 'Descripcion del reto',
-            'hint' => 'Pista para el reto',
-            'flag' => 'Respuesta del reto'
+            'name' => 'CriptoMensajes',
+            'description' => 'El reto trata de descriptar el mensaje en el siguiete mensaje',
+            'hint' => 'Cr_pt_',
+            'flag' => 'Cripto'
         ]);
         DB::table('challenges')->insert([
             'id' => 2,
             'idLevel' => 2,
-            'idCategory' => 2,
-            'name' => 'Nombre del Reto',
-            'description' => 'Descripcion del reto',
-            'hint' => 'Pista para el reto',
-            'flag' => 'Respuesta del reto'
-        ]);
-        DB::table('challenges')->insert([
-            'id' => 3,
-            'idLevel' => 3,
-            'idCategory' => 3,
-            'name' => 'Nombre del Reto',
-            'description' => 'Descripcion del reto',
-            'hint' => 'Pista para el reto',
-            'flag' => 'Respuesta del reto'
-        ]);
-        DB::table('challenges')->insert([
-            'id' => 4,
-            'idLevel' => 4,
-            'idCategory' => 4,
-            'name' => 'Nombre del Reto',
-            'description' => 'Descripcion del reto',
-            'hint' => 'Pista para el reto',
-            'flag' => 'Respuesta del reto'
-        ]);
-        DB::table('challenges')->insert([
-            'id' => 5,
-            'idLevel' => 5,
             'idCategory' => 5,
-            'name' => 'Nombre del Reto',
-            'description' => 'Descripcion del reto',
-            'hint' => 'Pista para el reto',
-            'flag' => 'Respuesta del reto'
+            'name' => 'Creada un procedimiento almacenado',
+            'description' => 'sintaxis para ingresar datos a una tabla',
+            'hint' => 'i_s_rt in_o',
+            'flag' => 'insert into'
         ]);
     }
 }
--- database/seeds/TeamsChallengesSeeder.php
+++ database/seeds/TeamsChallengesSeeder.php
@@ -11,42 +11,5 @@ class TeamsChallengesSeeder extends Seeder
      */
     public function run()
     {
-        $hours = new DateTime();
-        //
-        DB::table('teams_challenges')->insert([
-            'id' => 1,
-            'idteam' => 1,
-            'idChallenge' => 1,
-            'score' => 0,
-            'time' => $hours
-        ]);
-        DB::table('teams_challenges')->insert([
-            'id' => 2,
-            'idteam' => 2,
-            'idChallenge' => 2,
-            'score' => 0,
-            'time' => $hours
-        ]);
-        DB::table('teams_challenges')->insert([
-            'id' => 3,
-            'idteam' => 3,
-            'idChallenge' => 3,
-            'score' => 0,
-            'time' => $hours
-        ]);
-        DB::table('teams_challenges')->insert([
-            'id' => 4,
-            'idteam' => 4,
-            'idChallenge' => 4,
-            'score' => 0,
-            'time' => $hours
-        ]);
-        DB::table('teams_challenges')->insert([
-            'id' => 5,
-            'idteam' => 5,
-            'idChallenge' => 5,
-            'score' => 0,
-            'time' => $hours
-        ]);
-    }
+        /*SEMILLA DEL JUEGO */ }
 }
